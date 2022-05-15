c54218e48cec74c0179243b7f7a8b675590984e6
kylezparker <kyle.z.parker3@gmail.com>
2022-05-15 11:26:48 -0700

    creating variables
    we were creating variables with : let ,var ,and const

---
+++ script.js
@@ -0,0 +1,39 @@
+
+// alert("hello world");
+// prompt("enter your name");
+// console.log("this is a message");
+// console.warn("this is a warning");
+// console.error("this is an error");
+
+//variables
+var x; //declaration
+x=10; //assignation
+x=20;
+
+//name vs username
+var username= "kyle"; //dec and assign in same line
+
+console.log(username);
+console.log(x);
+
+let userAge = 98;
+console.log(userAge);
+document.writeln(x);
+document.writeln(` <p> the users name is : ${username}, and the age is ${userAge} <p> `);
+
+const taxes=11; //const doesnt change in program
+
+console.log(taxes);
+document.writeln(taxes);
+//document.writeln(userName);
+
+
+
+//ex
+let numChild= 5;
+let partnerName= "Jen";
+let geoLoc="Italy";
+let jobTitle="programmer";
+
+
+document.writeln(`<p>I will be a ${jobTitle} in ${geoLoc}, and married to ${partnerName} with ${numChild} kids. </p>`);
